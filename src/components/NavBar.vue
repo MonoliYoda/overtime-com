ee38555a7494a9c0a3e00e4fd291177de89111bf
MonoliYoda <48124492+MonoliYoda@users.noreply.github.com>
2021-01-03 17:41:24 +0100

    Added toglleable menu

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,11 +2,11 @@
     <nav>
     <ul class="menu">
         <li class="logo"><a>Overtime</a></li>
-        <li class="item"><input type="text"></li>
-        <li class="item"><a>Search</a></li>
-        <li class="item"><a>Settings</a></li>
-        <li class="item button secondary"><a @click="onLogin">Login</a></li>
-        <li class="toggle"><span class="bars"></span></li>
+        <li class="item" :class="{active: menuActive}"><input type="text"></li>
+        <li class="item" :class="{active: menuActive}"><a>Search</a></li>
+        <li class="item" :class="{active: menuActive}"><a>Settings</a></li>
+        <li class="item button secondary" :class="{active: menuActive}"><a @click="onLogin">Login</a></li>
+        <li class="toggle" @click="toggleMenu"><span class="bars"></span></li>
     </ul>
 
     </nav>
@@ -17,12 +17,20 @@ import firebase from 'firebase'
 // @click="onLogin"
 
 export default {
+    data() {
+        return {
+            menuActive: false
+        }
+    },
     methods: {
         onLogin() {
             var provider = new firebase.auth.GoogleAuthProvider();
             firebase.auth().signInWithPopup(provider).then(() => {
                 console.log('Signin Success!')
             })
+        },
+        toggleMenu() {
+            this.menuActive = !this.menuActive
         }
     }
 }
